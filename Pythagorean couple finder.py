f39c2255ac0bb96ed2c0c31aa353ead7062c707c
arezaei <83867047+TeraPascal@users.noreply.github.com>
2026-05-18 14:46:34 +0330

    Create Pythagorean couple finder.py

---
+++ Pythagorean couple finder.py
@@ -0,0 +1,12 @@
+import math as m
+z = lambda x, y: x**2 + y ** 2
+
+k = 0
+for i in range (1, 100):
+    for j in range(i, 100):
+        p = z(i, j)
+        n = m.sqrt(p)
+        if m.fabs(n-m.floor(n)) < .001 :
+            k += 1
+            print(i, j, m.floor(n))
+print('Number of pythogerean couple =',k)
